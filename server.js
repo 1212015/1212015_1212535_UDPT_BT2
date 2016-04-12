5e36dd48cef2353a6bace480026f6ac399f7f498
VuThiThanhXuan <1212535@student.hcmus.edu.vn>
2016-04-12 08:32:44 +0700

    Milestone 3 - Tạo trang home

--- server.js
+++ server.js
@@ -5,3 +5,15 @@ var app = express();
 app.listen(3000, function () {
   console.log('now listening on http://localhost:3000');
 })
+
+<!--cấu hình views-->
+var ect = require('ect');
+var ectRenderer = ect({ watch: true, root: __dirname + '/views', ext : '.ect' });
+app.set('view engine', 'ect');
+app.engine('ect', ectRenderer.render);
+
+<!--thêm routes vào server-->
+var routes = require('./routes');
+
+<!--Cấu hình routes trong server.js-->
+app.use('/', routes);
